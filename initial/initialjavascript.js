ad2940d91d95efddecdf488ecc8a76c529fe3d88
Cabral <rodrigo.c.cabral@accenture.com>
2019-12-30 16:42:41 -0300

    Gadalho 30.12.19 - 16:42

--- initial/initialjavascript.js
+++ initial/initialjavascript.js
@@ -100,23 +100,40 @@ function showSlides(n) {
 //-----------------Expandable sidebar--------------------//
 
 var mini = true;
+document.getElementById("myUL").style.visibility="hidden";   
 function toggleSidebar() {
+  
     if (mini) {
+      
         console.log("opening sidebar");
         document.getElementById("mySidebar").style.width = "252px";
-        document.getElementById("container-tela").style.marginLeft = "252px";
+        document.getElementById("container-tela").style.marginLeft = "210px";   
+        document.getElementById("myUL").style.visibility="visible";    
 
         this.mini = false;
     } else {
         console.log("closing sidebar");
         document.getElementById("mySidebar").style.width = "50px";
         document.getElementById("container-tela").style.marginLeft = "0px";
+        document.getElementById("myUL").style.visibility="hidden";
+
         this.mini = true;
     }
 }
+
 
 //------------CONVERSAO DE MOEDA TESTE-------------//
 
 
 //------------FIM DA CONVERSAO DE MOEDA TESTE------//
 
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+
+  });
+}
