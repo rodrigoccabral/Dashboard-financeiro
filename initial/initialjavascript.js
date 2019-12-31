790f7174895fd889910542247828fe4715be612e
Cabral <rodrigo.c.cabral@accenture.com>
2019-12-31 08:56:11 -0300

    teste

--- initial/initialjavascript.js
+++ initial/initialjavascript.js
@@ -1,102 +1,3 @@
-//---------------Controle do piechart-----------------//
-
-/*google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
-
-  var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work',     100],
-    ['Eat',      2],
-    ['Commute',  2],
-    ['Watch TV', 2],
-    ['Sleep',    7]
-  ]);
-
-  var options = {
-    title: 'My Daily Activities',
-    backgroundColor: { fill:'transparent' }
-  };
-
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-  chart.draw(data, options);
-}
-
-//---------------Controle do Columns chart-----------------//
-
-google.charts.load("current", {packages:['corechart']});
-google.charts.setOnLoadCallback(drawChart2);
-function drawChart2() {
-  var data = google.visualization.arrayToDataTable([
-    ["Element", "Density", { role: "style" } ],
-    ["Copper", 8.94, "#b87333"],
-    ["Silver", 10.49, "silver"],
-    ["Gold", 19.30, "gold"],
-    ["Platinum", 21.45, "color: #e5e4e2"]
-  ]);
-
-  var view = new google.visualization.DataView(data);
-  view.setColumns([0, 1,
-                   { calc: "stringify",
-                     sourceColumn: 1,
-                     type: "string",
-                     role: "annotation" },
-                   2]);
-
-  var options = {
-    title: "Density of Precious Metals, in g/cm^3",
-    width: 250,
-    height: 300,
-    bar: {groupWidth: "95%"},
-    legend: { position: "none" },
-    backgroundColor: { fill:'transparent' }
-
-  };
-  var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
-  chart.draw(view, options);
-}
-
-//---------------Controle do Lines chart-----------------//
-
-
-//---------------Carrossel---------------------//
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if(n < 1) {
-        slideIndex = slides.length;
-    }
-    for(var i = 0;i < slides.length;i++) {
-        slides[i].style.display = "none";
-    }
-    for(var i = 0;i < dots.length;i++) {
-        dots[i].className = dots[i].className.replace(" active", "")
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-
-}*/
-
 //-----------------Expandable sidebar--------------------//
 
 var mini = true;
@@ -121,11 +22,12 @@ function toggleSidebar() {
     }
 }
 
-
 //------------CONVERSAO DE MOEDA TESTE-------------//
 
 
 //------------FIM DA CONVERSAO DE MOEDA TESTE------//
+
+//------------TREE VIEW SIDEBAR-------------//
 
 var toggler = document.getElementsByClassName("caret");
 var i;
@@ -137,3 +39,4 @@ for (i = 0; i < toggler.length; i++) {
 
   });
 }
+//------------FIM DA TREE VIEW SIDEBAR-------------//
